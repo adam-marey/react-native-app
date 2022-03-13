afa406054809327e4282171c96135d1278604234
Sulaiman Marey <adammarey51@gmail.com>
2022-03-13 16:21:50 -0700

    added imageInput component

--- bookstore/App.js
+++ bookstore/App.js
@@ -1,32 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useEffect } from 'react';
+// import { useEffect } from 'react/cjs/react.production.min';
+import Screen from './app/components/Screen';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
+export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.getMediaLibraryPermissionsAsync();
+    if (!granted) alert('You need to enale permission to access the library');
   };
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
-    </View>
-  );
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
