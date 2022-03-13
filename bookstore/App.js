90b1cb8ebf432f006296750b00a255e856237e02
Sulaiman Marey <adammarey51@gmail.com>
2022-03-13 01:57:56 -0700

    added pickerItem and AppPicker components

--- bookstore/App.js
+++ bookstore/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import Screen from './app/components/Screen';
-// import Icon from './app/components/Icon';
-// import ListItem from './app/components/ListItem';
-// import AccountScreen from './app/screens/AccountScreen';
-// import ListingsScreen from './app/screens/ListingScreen';
-// import Screen from './app/components/Screen';
-// import ListItemDeleteAction from './app/components/ListItemDeleteAction';
-// import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import Screen from './app/components/Screen';
+import AppPicker from './app/components/AppPicker';
 import AppTextInput from './app/components/AppTextInput';
-// import MessagesScreen from './app/screens/MessagesScreen';
+
+const categories = [
+  { label: 'Books', value: 1 },
+  { label: 'Novels', value: 2 },
+  { label: 'Magazines', value: 3 },
+];
 
 export default function App() {
-  return <AppTextInput />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
