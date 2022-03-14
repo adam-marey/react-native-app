5686b1808c8e0ce6b6ab1f4c9173f7d76455075f
Sulaiman Marey <adammarey51@gmail.com>
2022-03-13 18:50:18 -0700

    added routes file

---
+++ bookstore/app/navigation/routes.js
@@ -0,0 +1,8 @@
+export default Object.freeze({
+  // this method used to make sure these routes can not be motified any where in the app.
+  LISTING_DETAILS: 'ListingDetails',
+  LISTING_EDIT: 'ListingEdit',
+  LOGIN: 'Login',
+  MESSAGES: 'Messages',
+  REGISTER: 'Register',
+});
