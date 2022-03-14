8906ff8faaee7ce3efc4a959a738a4704bdd1e84
Sulaiman Marey <adammarey51@gmail.com>
2022-03-13 17:27:10 -0700

    added useLocation hooks

--- bookstore/App.js
+++ bookstore/App.js
@@ -1,16 +1,5 @@
-import React, { useEffect } from 'react';
-// import { useEffect } from 'react/cjs/react.production.min';
-import Screen from './app/components/Screen';
-import * as ImagePicker from 'expo-image-picker';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.getMediaLibraryPermissionsAsync();
-    if (!granted) alert('You need to enale permission to access the library');
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-  return <Screen></Screen>;
+  return <ListingEditScreen />;
 }
